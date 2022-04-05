48bc474bde8ded107655060082741093e3f00e53
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2022-04-05 19:29:21 -0300

    feat: add CarouselSlide WIP

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,10 @@
 import React from "react"
+import CarouselSlide from "./components/carrossel/index"
 
-const App = () => {}
+const App = () => (
+  <>
+    <CarouselSlide />
+  </>
+)
 
 export default App
---
+++ app/src/components/carrossel/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Carousel from 'react-elastic-carousel'
+
+const CarouselSlide = () => (
+  <Carousel itemsToShow={1}>
+    <p>a</p>
+    <p>b</p>
+  </Carousel>
+)
+
+export default CarouselSlide
